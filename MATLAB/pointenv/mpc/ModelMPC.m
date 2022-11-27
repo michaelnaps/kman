b0e51e4ec2e25196db46ce65f96f4449f30a6dec
Michael Napoli <mikednapoli@gmail.com>
2022-11-27 17:39:36 -0500

    beginning of MPC formulation from notes

--- MATLAB/pointenv/mpc/ModelMPC.m
+++ MATLAB/pointenv/mpc/ModelMPC.m
@@ -1,3 +1,28 @@
-function [u] = ModelMPC(xG, x0, world)
+function [u] = ModelMPC(Np, x0, xG, uref, world)
+    
+    Nx = length(x0);
+    Nu = length(uref);
+    Nw = length(world);
+
+%     Q = eye(Nx);
+%     P = eye(Nu);
+%     R = eye(Nx);
+
+    H = eye(Np*(Nx+Nu)+Nx, Np*(Nx+Nu)+Nx);
+
+    x = NaN(1, Np*Nx);
+    x(1:4) = x0;
+
+    p = 5;
+    for i = 2:Np
+
+       x(p:p+Nx-1) = model(x(p-Nx:p-1), uref);
+       p = p + Nx;
+
+    end
+
+    x'
+
+    u = [0,0];
 
 end
---
+++ MATLAB/pointenv/mpc/root.m
@@ -0,0 +1,45 @@
+clean;
+
+
+%% add paths and read in data
+addpath ../.
+addpath ../../.
+addpath ../sphereworld
+addpath ./data
+
+load sphereworld world;
+Nw = length(world);
+
+load K_19x19
+
+
+%% time parameters
+Np = 10;
+T = 30;  Nt = T/dt + 1;
+tspan = (0:dt:T)';
+
+
+%% create test environment
+x0 = [0, -8, 0, 0];
+xG = [5, 6];
+Nx = length(x0);
+uref = [0, 0];
+
+controlledModel = @(x) model(x, ModelMPC(Np, x0, xG, uref, world), dt);
+controlledModel(x0);
+
+
+%% run simulation
+% xm = NaN(Nt, Nx);
+% xm(1,:) = x0;
+% for i = 2:Nt
+%     xm(i,:) = controlledModel(xm(i-1,:));
+% end
+% 
+% bernard = struct;
+% bernard.xCenter = [0,0];
+% bernard.radius = 0.25;
+% bernard.distInfluence = 0.25;
+% bernard.color = 'k';
+% 
+% [~] = plot_path(bernard, xm, world, xG);
