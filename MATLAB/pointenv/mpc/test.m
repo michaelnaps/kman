cfd254bde79d0068cd4afb9d935e3caf3c97655d
Michael Napoli <mikednapoli@gmail.com>
2022-12-01 22:49:14 -0500

    trying new form of propagation function

--- MATLAB/pointenv/mpc/test.m
+++ MATLAB/pointenv/mpc/test.m
@@ -20,27 +20,40 @@ load K_11x11
 Nt = 100;
 xU = [0,0,0,0];
 uX = [0,0];
-obsFun = @(x,u) observables(x, u, world, Q);
+obsFun = @(x, u) observables(x, u, world, Q);
 
 
 %% test linear combination
 TOL = 1e-6;
 x0 = [0,0,0,0];
+Psi0 = obsFun(x0, [0,0]);
 u  = [0.3,0.5];
+uPsi = obsFun([0,0,0,0], u);
+
+
+%% dimension variables
 Nx = length(x0);
+Nu = length(u);
+Nk = length(K);
+
+
+%% koopman operator modification
 Kx = K(:,1:Nx);
+Ku = K(:,Nx+1:Nx+Nu);
 
-x_stnd = NaN(Nt, Nx);
-x_comb = NaN(Nt, Nx);
 
-x_stnd(1,:) = x0;
-x_comb(1,:) = x0;
+%% state matrices
+x = NaN(Nt, Nx);
+xPsi = NaN(Nt, Nk);
+
+x(1,:) = x0;
+xPsi(1,:) = Psi0;
 
 for i = 1:Nt-1
-    x_stnd(i+1,:) = obsFun(x_stnd(i,:), u)*Kx;
-    x_comb(i+1,:) = obsFun(x_comb(i,:), uX)*Kx + obsFun(xU, u)*Kx;
+    x(i+1,:) = obsFun(x(i,:), u)*Kx;
+    xPsi(i+1,:) = xPsi(i,:)*K + uPsi*K;
 
-    disp(sum(x_stnd(i+1,:)-x_comb(i+1,:) < TOL , 'all'))
+    disp(sum(x(i+1,:)-xPsi(i+1,1:Nx) < TOL, 'all') == 4)
 end
 
-disp([x_stnd, NaN(Nt,1), x_comb])
+disp([x, NaN(Nt,1), xPsi(:,1:Nx)])
