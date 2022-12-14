6ddae687353aecd3c47d6e12cadd7b68e964b6b4
Michael Napoli <mikednapoli@gmail.com>
2022-12-14 12:13:44 -0500

    ready to save save analytical results for report

--- MATLAB/pointenv/analytical/root.m
+++ MATLAB/pointenv/analytical/root.m
@@ -31,11 +31,11 @@ Nt = length(tspan);
 % create list of inputs
 A = 5;
 % u0 = A*rand(1,Nu) - A/2;
-% uList = u0.*ones(Nt-1,Nu);               % constant input
+% uList = u0.*ones(Nt-1,Nu);                 % constant input
 % uList = u0 + (0.50*rand(Nt-1,Nu) - 0.25);  % input with noise
-uList = A*[
-    cos(linspace(0, 4*pi, Nt-1)'), cos(linspace(0, 4*pi, Nt-1)')
-] - A/2;
+uList = A*[                                  % sinusoidal input
+    cos(linspace(0, 6*pi, Nt-1)'), cos(linspace(0, 4*pi, Nt-1)')
+];
 
 
 %% Evaluate for the observation function
