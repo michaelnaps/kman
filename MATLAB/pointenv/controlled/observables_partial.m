afa7082e83a285b4adb5649a83d160d55ca05539
Michael Napoli <mikednapoli@gmail.com>
2022-12-17 14:54:53 -0500

    reorganized from report files

--- MATLAB/pointenv/controlled/observables_partial.m
+++
@@ -1,7 +0,0 @@
-function [dPsix, dPsiu] = observables_partial(x, u)
-    obs_x = @(x) observables(x, u);
-    dPsix = naps.fdm(obs_x, x);
-
-    obs_u = @(u) observables(x, u);
-    dPsiu = naps.fdm(obs_u, u);
-end
