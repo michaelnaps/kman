b02318036f516aea9c7fc137573c00e783daddb3
Michael Napoli <mikednapoli@gmail.com>
2023-01-24 15:19:45 -0500

    began python conversion

--- Python/KoopFunctions/observables_partial.m
+++
@@ -1,9 +0,0 @@
-function [dPsix, dPsiu] = observables_partial(x, u, world)
-
-    obs_x = @(x) observables(x, u, world);
-    dPsix = fdm(obs_x, x);
-
-    obs_u = @(u) observables(x, u, world);
-    dPsiu = fdm(obs_u, u);
-
-end
