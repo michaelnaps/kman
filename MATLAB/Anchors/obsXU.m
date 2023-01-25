ac00afca166a289bdfc10b3574fcfb153e7f424d
Michael Napoli <mikednapoli@gmail.com>
2023-01-25 17:20:14 -0500

    testing new K formulation

--- MATLAB/Anchors/obsXU.m
+++ MATLAB/Anchors/obsXU.m
@@ -1,8 +1,9 @@
-function [PsiXU, metaXU] = obsXU(x, u)  
+function [PsiXU, metaXU] = obsXU(x)  
     [PsiX, metaX] = obsX(x);
     [PsiU, metaU] = obsU(x);
+    [PsiH, metaH] = obsH([x;0;0]);
 
-    PsiXU = [PsiX; PsiU*u];
+    PsiXU = [PsiX; kron(PsiU, PsiH)];
 
     labels = fieldnames(metaU);
 
@@ -10,4 +11,6 @@ function [PsiXU, metaXU] = obsXU(x, u)
     for i = 1:length(labels)
         metaXU.(labels{i}) = metaU.(labels{i}) + metaXU.Nk;
     end
+
+    metaXU.Nk = metaX.Nk*(1 + metaH.Nk);
 end
