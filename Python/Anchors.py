81a48e821e142c20794abb85db4430f98b216b1f
Michael Napoli <mikednapoli@gmail.com>
2023-02-04 20:48:31 -0500

    solved for Ku and cumulative operator, untested but look good

--- Python/Anchors.py
+++ Python/Anchors.py
@@ -4,15 +4,45 @@ import matplotlib.pyplot as plt
 import Helpers.KoopmanFunctions as kman
 
 
-def obsX(x):
+# create global "measurement" variables
+Nu = 2;
+Na = 4;
+anchors = np.random.rand(2,Na);
+
+
+def obsX(x=None):
+    if x is None:
+        meta = {'Nk':4};
+        return meta;
     return x;
 
-def obsU(x):
+def obsU(x=None):
+    if x is None:
+        meta = {'Nk':1};
+        return meta;
     return 1;
+
+def obsH(X=None):
+    if X is None:
+        meta = {'Nk':Na+Nu};
+        return meta;
+
+    x = X[:4];
+    u = X[4:];
+
+    xp = x[:2].T[0];
+
+    dist = np.zeros( (Na,1) );
+    for i, anchor in enumerate(anchors.T):
+        dist[i] = (anchor.T - xp).T @ (anchor.T - xp);
+
+    Psi = np.vstack( (dist, u) );
+
+    return Psi;
 
 def obs(X=None):
     if X is None:
-        meta = {'Nk': 6};
+        meta = {'Nk': obsX()['Nk'] + obsU()['Nk']*2};
         return meta;
 
     x = X[:4];
@@ -26,6 +56,13 @@ def obs(X=None):
 
     return Psi;
 
+def plot(T, X, U):
+    fig, axs = plt.subplots(2,1);
+    axs[0].scatter(X[0], X[1]);
+    axs[1].plot(T, U[0]);
+    axs[1].plot(T, U[1]);
+    return fig, axs;
+
 
 # set global output setting
 np.set_printoptions(precision=3, suppress=True);
@@ -34,9 +71,9 @@ np.set_printoptions(precision=3, suppress=True);
 if __name__ == "__main__":
     # construct model
     Nx = 4;
-    Nu = 2;
 
     dt = 0.1;
+    xg = np.zeros( (Nx,1) );
     A = [
         [1, 0, dt, 0],
         [0, 1, 0, dt],
@@ -49,8 +86,14 @@ if __name__ == "__main__":
         [dt, 0],
         [0, dt]
     ];
+    C = [
+        [10, 0, 2.5, 0],
+        [0, 10, 0, 2.5]
+    ];
 
     model = lambda x,u: A@x + B@u;
+    control = lambda x: C@(xg.reshape(Nx,1) - x.reshape(Nx,1));
+
 
     # simulation variables
     T = 10;  Nt = round(T/dt)+1;
@@ -67,12 +110,9 @@ if __name__ == "__main__":
     for i in range(Nt-1):
         xTrain[:,i+1] = model(xTrain[:,i], uRand[:,i]);
 
-    # plot the trajectories
-    fig, axs = plt.subplots(2,1);
-    axs[0].plot(xTrain[0], xTrain[1]);
-    axs[1].plot(tTrain[:Nt-1], uRand[0]);
-    axs[1].plot(tTrain[:Nt-1], uRand[1]);
-    # plt.show();
+    plot(tTrain[:Nt-1], xTrain, uRand);
+    # plt.show()
+
 
     # construct Kx data matrices
     X = np.vstack( (xTrain[:,:Nt-1], uRand) );
@@ -80,7 +120,45 @@ if __name__ == "__main__":
 
     X0 = np.vstack( (x0, uRand[:,0].reshape(Nu,1)) );
 
-    kvar = kman.KoopmanOperator(obs);
-    Kx = kvar.edmd(X, Y, X0);
+    kxvar = kman.KoopmanOperator(obs);
+    Kx = kxvar.edmd(X, Y, X0);
 
-    print(Kx);
+    print('Kx\n', Kx);
+
+
+    # construct data for Ku
+    xRand = np.random.rand(Nx,Nt-1);
+    uTrain = np.zeros( (Nu,Nt-1) );
+
+    for i in range(Nt-1):
+        uTrain[:,i] = control(xRand[:,i]).reshape(Nu,);
+
+    plot(tTrain[:Nt-1], xRand, uTrain);
+    # plt.show()
+
+
+    # solve for Ku
+    Xu = np.vstack( (xRand, np.zeros( (Nu,Nt-1) )) );
+    Yu = np.vstack( (xRand, uTrain) );
+
+    Xu0 = np.vstack( (
+        xRand[:,0].reshape(Nx,1), uTrain[:,0].reshape(Nu,1)
+    ) );
+    kuvar = kman.KoopmanOperator(obsH);
+    Ku = kuvar.edmd(Xu, Yu, Xu0)
+
+    print('Ku\n', Ku);
+
+
+    # calculate cumulative operator
+    m = Nu;
+    p = obsX()['Nk'];
+    q = obsU()['Nk'];
+    b = obsH()['Nk'];
+
+    K = Kx @ np.vstack( (
+        np.hstack( (np.eye(p), np.zeros( (p,q*b) )) ),
+        np.hstack( (np.zeros( (m*q, p) ), np.kron(np.eye(q), Ku[4:,:])) )
+    ) );
+
+    print('K\n', K)
