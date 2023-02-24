3714406405f5f6536a9cf89b52177d8a897ef167
Michael Napoli <mikednapoli@gmail.com>
2023-02-24 15:55:18 -0500

    added cool PH anim

--- donald.py
+++ donald.py
@@ -20,8 +20,9 @@ R = 1/2;  # robot-body radius
 dt = 0.01;
 
 
+# callback function and parameters
 class Parameters:
-    def __init__(self, x0, xd,
+    def __init__(self, x0, xd, PH,
                  fig=None, axs=None,
                  buffer_length=10, pause=1e-3,
                  color='k'):
@@ -39,6 +40,7 @@ class Parameters:
         self.fig.tight_layout();
 
         # initialize buffer (trail)
+        self.PH = PH;
         self.color = color;
         self.width = 0.10;
         self.length = R/2;
@@ -51,6 +53,9 @@ class Parameters:
         self.buffer = [x0[:2] for i in range(buffer_length)];
         self.trail_patch = patch.PathPatch(path.Path(self.buffer), color=self.color);
 
+        self.prediction = [x0[:2] for i in range(self.PH+1)];
+        self.future_patch = patch.PathPatch(path.Path(self.buffer), color='C1');
+
         m1 = self.length*math.cos(x0[2]);
         m2 = self.length*math.sin(x0[2]);
         self.orientation = patch.Arrow(x0[0], x0[1], m1, m2,
@@ -58,19 +63,26 @@ class Parameters:
 
         self.axs.add_patch(permanentPatch);
         self.axs.add_patch(self.trail_patch);
+        self.axs.add_patch(self.future_patch)
         self.axs.add_patch(self.orientation);
 
         self.pause = pause;
         self.xd = xd;
 
-    def update(self, t, x):
+    def update(self, t, x, xPH):
         self.trail_patch.remove();
+        self.future_patch.remove();
         self.orientation.remove();
 
         self.buffer[:-1] = self.buffer[1:];
         self.buffer[-1] = x[:2];
 
-        self.trail_patch = patch.PathPatch(path.Path(self.buffer), fill=0);
+        self.trail_patch = patch.PathPatch(path.Path(self.buffer),
+            color=self.color, fill=0);
+
+        self.prediction = [xPH[i][:2] for i in range(self.PH+1)];
+        self.future_patch = patch.PathPatch(path.Path(self.prediction),
+            color='C1', fill=0);
 
         dx1 = self.length*math.cos(x[2]);
         dx2 = self.length*math.sin(x[2]);
@@ -78,6 +90,7 @@ class Parameters:
                                        width=self.width, color=self.color);
 
         self.axs.add_patch(self.trail_patch);
+        self.axs.add_patch(self.future_patch);
         self.axs.add_patch(self.orientation);
 
         plt.show(block=0);
@@ -86,15 +99,18 @@ class Parameters:
         return self;
 
 def callback(mpc_var, T, x, u):
-    return mpc_var.params.update(T, x);
+    xPH = mpc_var.simulate(x, u);
+    return mpc_var.params.update(T, x, xPH);
 
+
+# model functions
 def model(x, u, _):
-    dx = [
-        math.cos(x[2])*(u[0] + u[1]),
-        math.sin(x[2])*(u[0] + u[1]),
-        1/R*(u[0] - u[1])
+    xn = [
+        x[0] + dt*math.cos(x[2])*(u[0] + u[1]),
+        x[1] + dt*math.sin(x[2])*(u[0] + u[1]),
+        x[2] + dt*1/R*(u[0] - u[1])
     ]
-    return dx;
+    return xn;
 
 def cost(mpc_var, xlist, ulist):
     # grab class variables
@@ -132,18 +148,28 @@ def cost(mpc_var, xlist, ulist):
     return C;
 
 
+# observable functions
+def obs(x=None):
+    if x is None:
+        meta = {'Nk':0};
+        return meta;
+    Psi = np.vstack( (x, 1) );
+    return Psi;
+
+
 if __name__ == "__main__":
     # initialize states
     x0 = [0,0,pi/2];
     xd = [1,1,3*pi/2];
 
     # create MPC class variable
-    PH = 5;
+    PH = 10;
     kl = 2;
-    model_type = 'continuous';
-    params = Parameters(x0, xd, buffer_length=25);
+    model_type = 'discrete';
+    params = Parameters(x0, xd, PH, buffer_length=25, pause=0.1);
     mpc_var = mpc.ModelPredictiveControl('ngd', model, cost, params, Nu,
-        num_ssvar=Nx, PH_length=PH, knot_length=kl, time_step=dt, model_type=model_type);
+        num_ssvar=Nx, PH_length=PH, knot_length=kl, time_step=dt,
+        max_iter=10, model_type=model_type);
     mpc_var.setAlpha(0.01);
 
     # solve single time-step
