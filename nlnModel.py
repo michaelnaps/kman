13e2c2c959e9504bc58c35f2deb07ba7b36d2ad2
Michael Napoli <mikednapoli@gmail.com>
2023-02-24 14:39:43 -0500

    need to see what it means to properly control the Van der Pol equation

--- nlnModel.py
+++ nlnModel.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, '/home/michaelnaps/prog/mpc');
 
+import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as patch
 import matplotlib.path as path
@@ -11,28 +12,80 @@ import Helpers.DataFunctions as data
 
 
 # hyper parameter(s)
+PH = 2;
+xd = [0,2];
 Nx = 2;
 Nu = 1;
-dt = 0.01;
+dt = 0.025;
+
+
+# callback function for runtime
+class Parameters:
+    def __init__(self, x0,
+                 fig=None, axs=None,
+                 buffer_length=10, pause=1e-3,
+                 color='k'):
+        if axs is None and fig is None:
+            self.fig, self.axs = plt.subplots();
+        else:
+            self.fig = fig;
+            self.axs = axs;
+
+        self.axs.plot(xd[0], xd[1], color='g', marker='*', markersize=5)
+        self.axs.set_xlim(-3, 3);
+        self.axs.set_ylim(-4, 4);
+        self.fig.tight_layout();
+
+        # initialize buffer (trail)
+        self.color = color;
+
+        self.buffer = [x0 for i in range(buffer_length)];
+        self.trail_patch = patch.PathPatch(path.Path(self.buffer), color=self.color);
+
+        self.axs.add_patch(self.trail_patch);
+
+        self.pause = pause;
+
+    def update(self, t, x):
+        self.trail_patch.remove();
+
+        self.buffer[:-1] = self.buffer[1:];
+        self.buffer[-1] = x;
+
+        self.trail_patch = patch.PathPatch(path.Path(self.buffer), fill=0);
+        self.axs.add_patch(self.trail_patch);
+
+        plt.show(block=0);
+        plt.pause(self.pause);
+
+        return self;
+
+def callback(mvar, T, x, u):
+    return mvar.params.update(T, x);
 
 
 # set global output setting
-np.set_printoptions(precision=3, suppress=True);
-
-def modelFunc(x, u, _):
+def model(x, u, _):
     mu = 1;  a = 1;
     x1 = x[0];  x2 = x[1];
     u = u[0];
 
     xn = [
         x1 + dt*x2,
-        x2 + dt*(mu*(1 - x1**2)*x2 - x1 + a*np.sin(u))
+        x2 + dt*(mu*(1 - x1**2)*x2 - x1 + a*math.sin(u))
     ];
 
     return xn;
 
 def cost(mvar, xlist, ulist):
-    pass;
+    kx = 100;
+    kv = 1;
+    g = 0;
+
+    for x in xlist:
+        g += kx*(xd[0] - x[0])**2 + kv*(xd[1] - x[1])**2;
+
+    return g;
 
 
 # def obsX(x=None):
@@ -50,12 +103,24 @@ def cost(mvar, xlist, ulist):
 #     return Psi;
 #
 # def obsH(x=None):
-    if x is None:
-        meta = {'Nk':3};
-        return meta;
-    Psi = np.vstack( (x[2], np.sin(x[2]), x[0]**2) );
-    return Psi;
+#     if x is None:
+#         meta = {'Nk':3};
+#         return meta;
+#     Psi = np.vstack( (x[2], np.sin(x[2]), x[0]**2) );
+#     return Psi;
 
 
 if __name__ == "__main__":
-    pass;
+    # test case
+    x0 = [1, 0];
+    uinit = [0 for i in range(Nu*PH)];
+
+    # create MPC class variable
+    params = Parameters(x0, buffer_length=100);
+    type = 'discrete';
+    mvar = mpc.ModelPredictiveControl('ngd', model, cost, params, Nu,
+        num_ssvar=Nx, PH_length=PH, time_step=dt, model_type=type);
+
+    # simulate test case
+    sim_time = 10;
+    xlist = mvar.sim_root(sim_time, x0, uinit, callback=callback, output=1)[1];
