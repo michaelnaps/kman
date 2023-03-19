doing something fundamentally wrong in calculation of Kx

Helpers/KoopmanFunctions.py
@@ -41,7 +41,7 @@ def bcd(Klist, flist, X, Y, X0, TOL=1e-3):
     # operator dimensions
     N = len(Klist);
     (N0, Nt, _, _) = dimnData(X, X0);
-    
+
     # lift data into the appropriate function spaces
     Glist = [None for i in range(N)];
     Alist = Glist;
@@ -59,11 +59,9 @@ def bcd(Klist, flist, X, Y, X0, TOL=1e-3):
             NkY = Klist[i].metaY['Nk'];
 
             M = f(Klist, Glist[i]);
+            
+            Klist[i].K = nvec( np.linalg.lstsq(Alist[i], M)[0], NkX, NkY );
 
-            print(M.shape);
-            print(Alist[i].shape)
-
-            Klist[i].K = nvec( np.linalg.lstsq(M, Alist[i]), NkX, NkY );
 
     Kl = None;
     return Kl;
@@ -85,7 +83,6 @@ class KoopmanOperator:
                 self.obsY = self.obsX;
             else:
                 self.obsY = lambda y=None: obsY(y, params);
-
 
         # Koopman parameters
         self.metaX = self.obsX();

bcdpoint.py
@@ -61,7 +61,7 @@ def obsU(X=None):
     PsiU = [1];
     return PsiU;
 
-def PsiXU(X=None):  # proabably don't need
+def obsXU(X=None):  # proabably don't need
     if X is None:
         meta = {'Nk':obsX()['Nk']+obsU()['Nk']};
         return meta;
@@ -127,6 +127,8 @@ if __name__ == "__main__":
     q = obsU()['Nk'];
     b = obsH()['Nk'];
 
+    print(m, p, q, b);
+
 
     # construct matrices functions
     def Kblock(Ku):
@@ -142,11 +144,14 @@ if __name__ == "__main__":
         M = np.kron( PsiX.T, Klist[0].K );
         return M;
 
+    def Mtemp(Klist, PsiX):
+        M = Klist[0].K @ PsiX;
+        return M;
 
     # initialize operator class
-    kuvar = KoopmanOperator(obsH);
+    kuvar = KoopmanOperator(obsXUH);
     kxvar = KoopmanOperator(obsXUH);
-    bcd((kxvar, kuvar), (Mx, Mu), X, Y, X0);
+    bcd((kxvar,), (Mtemp,), X, Y, X0);
 
 
     # # new operator model equation