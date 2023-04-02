9a8401731d02fd64c87a6d2c939c2a2a056a801e
Michael Napoli <mikednapoli@gmail.com>
2023-04-01 21:48:39 -0400

    vect != true - maybe matrices orientations?

--- .algebra/m17/root.py
+++ .algebra/m17/root.py
@@ -1,59 +1,67 @@
 import numpy as np
 
-np.set_printoptions(precision=2, suppress=True);
-
-Nt=10
-NkH=2 #nb of measurements from the robot
-NkU=4
-NkX=4
-Nu=3 #nb of inputs for the robot
-
-h=np.random.rand(NkH, Nt)
-PsiX=np.random.rand(NkX, Nt)
-Psin=np.random.rand(NkX+NkU*NkH, Nt)
-PsiU=np.random.rand(NkU, Nt)
-
-Ku=np.random.rand(Nu, NkH)
-Kx=np.random.rand(NkX+NkH*NkU, NkX+NkH*NkU)
-
 def vec(A):
-    (n,m) = A.shape
-    return A.reshape(n*m,1)
+    (n,m) = A.T.shape;
+    return A.T.reshape(n*m,1);
+
+# dimension variables and environment setup
+m = 2;
+p = 5;
+q = 3;
+b = 6;
+Nt = 2;
+
+PsiX = np.random.rand(p,Nt);
+PsiU = np.random.rand(q,Nt);
+PsiH = np.random.rand(b,Nt);
+
+Psi1 = np.empty( (q*b,Nt) );
+for i in range(Nt):
+    Psi1[:,i] = np.kron(PsiU[:,i], PsiH[:,i]);
+Psi1 = np.vstack( (PsiX, Psi1) );
+Psi2 = np.random.rand(p+q*b,Nt);
+
+Kx = np.random.rand(p+q*b, p+q*b);
+Ku = np.random.rand(b,b);
 
 def Kblock(K):
-    Kb = np.vstack( (
-        np.hstack( (np.eye(NkX), np.zeros( (NkX, NkH*NkU) )) ),
-        np.hstack( (np.zeros( (NkU*Nu, NkX) ), np.kron(np.eye(NkU), K)) )
+    M = np.vstack( (
+        np.hstack( (np.eye(p), np.zeros( (p,q*b) )) ),
+        np.hstack( (np.zeros( (q*b,p) ), np.kron(np.eye(q), Ku)) )
     ) );
-    return Kb;
+    return M;
 
-def cost_matrix_form(Ku):
-    Mu=Kblock(Ku);
-    PsiUH = np.empty( (NkU*NkH, Nt) ) # matrix where each column is PsiU kron h
-    for i_data in range(Nt):
-        PsiUH[:,i_data]=np.kron(PsiU[:,i_data], h[:,i_data])
-    return np.linalg.norm( Psin - Mu@np.vstack( (PsiX,PsiUH) ) )
+# true cost form
+def trueCost():
+    Mu = Kblock(Ku);
+    PsiPlus = Kx@Mu@Psi1;
+    return np.linalg.norm( Psi2 - PsiPlus );
 
-def cost_vector_form(Ku):
-    Kxleft=Kx[:,:NkX]
-    Kxright=Kx[:,NkX:]
-    d=vec(Psin - Kxleft@PsiX)
-    C_list=[]
-    for i_data in range(Nt):
-        M_i_data_list=[]
-        for p in range(NkH):
-            for q in range(NkH):
-                epq=np.zeros( (NkH,NkH) )
-                epq[p,q] = 1
-                M_i_data_column=np.kron(PsiU[:,i_data],epq@h)
-                M_i_data_list.append(M_i_data_column)
-        M_i_data=np.hstack(M_i_data_list)
-        C_list.append(M_i_data)
-    C=np.vstack(C_list)
-    print(Ku)
+# vectorized cost form
+def vectCost():
+    Kxl = Kx[:,:p];
+    Kxr = Kx[:,p:];
+    Psi2Right = vec(Kxl@PsiX);
 
-    print(C.shape, vec(Ku).shape, d.shape)
-    return np.linalg.norm(C@vec(Ku)-d)
+    c = 0;
+    Clist = np.empty( (Nt*(p+q*b), b*b) );
+    for k in range(Nt):
+        s = 0;
+        Mlist = np.empty( (p+q*b,b*b) );
+        for i in range(b):
+            ei = np.array( [[1*(i==l)] for l in range(b)] );
+            for j in range(b):
+                ej = np.array( [[1*(j==l)] for l in range(b)] );
+                M = np.kron(PsiU[:,k,None], ei@ej.T@PsiH[:,k,None])
+                M = np.vstack( (PsiX[:,k,None], M) );
+                Mlist[:,s] = M[:,0];
+                s += 1;
+        Clist[c:c+(p+q*b),:] = Mlist;
+        c += p+q*b;
 
-print( cost_matrix_form(Ku) );
-print( cost_vector_form(Ku) );
+    Psi2Left = Clist@vec(Ku);
+    return np.linalg.norm( vec(Psi2) - Psi2Left - Psi2Right );
+
+# comparison
+print( trueCost() );
+print( vectCost() );
