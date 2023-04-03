bd786bd7f604f79db52765995327be51af4a2724
Michael Napoli <mikednapoli@gmail.com>
2023-04-03 13:41:40 -0400

    working on alg

--- .algebra/m17/root.py
+++ .algebra/m17/root.py
@@ -5,11 +5,13 @@ def vec(A):
     return A.T.reshape(n*m,1);
 
 # dimension variables and environment setup
-m = 2;
-p = 5;
-q = 3;
-b = 6;
+m = 3;
+p = 2;
+q = 6;
+b = 5;
 Nt = 2;
+
+print(p+q*b);
 
 PsiX = np.random.rand(p,Nt);
 PsiU = np.random.rand(q,Nt);
@@ -44,20 +46,22 @@ def vectCost():
     Psi2Right = vec(Kxl@PsiX);
 
     c = 0;
-    Clist = np.empty( (Nt*(p+q*b), b*b) );
+    skip = q*b;
+    Clist = np.empty( (Nt*(skip), b*b) );
     for k in range(Nt):
         s = 0;
-        Mlist = np.empty( (p+q*b,b*b) );
+        Mlist = np.empty( (skip,b*b) );
         for i in range(b):
             ei = np.array( [[1*(i==l)] for l in range(b)] );
             for j in range(b):
                 ej = np.array( [[1*(j==l)] for l in range(b)] );
-                M = np.kron(PsiU[:,k,None], ei@ej.T@PsiH[:,k,None])
-                M = np.vstack( (PsiX[:,k,None], M) );
+                M = np.kron(PsiU[:,k,None], ei@ej.T@PsiH[:,k,None]);
                 Mlist[:,s] = M[:,0];
                 s += 1;
-        Clist[c:c+(p+q*b),:] = Mlist;
-        c += p+q*b;
+        Clist[c:c+skip,:] = Mlist;
+        c += skip;
+
+    print(Clist.shape);
 
     Psi2Left = Clist@vec(Ku);
     return np.linalg.norm( vec(Psi2) - Psi2Left - Psi2Right );
