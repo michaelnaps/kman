229e1fc9c761b6468e8d6e88ad2e0eea11ca6b7e
Michael Napoli <mikednapoli@gmail.com>
2023-05-20 22:03:50 -0400

    re is low, no results displayed - kxvar deprecated until update in obs functions

--- cascade.py
+++ cascade.py
@@ -1,7 +1,7 @@
 # script for model equation testing
 from diffdrive import *
 
-def controlFlow(u, x0):
+def flow(u, x0):
     g = np.array( mpc_var.gradient(x0, u) );
     un = np.array( u ).reshape(Nu*PH,1) - alpha*g.reshape(Nu*PH,1);
     return un;
@@ -41,7 +41,7 @@ def createControlSets(iList, X0):
     xTrain = np.empty( (N0*Nx, Ni-1) );
     for k in range(N0):
         control = lambda u: X0[:,k,None];  # position treated as constant 'control'
-        uTemp, xTemp = data.generate_data(iList, controlFlow, U0[:,k,None], control, Nx);
+        uTemp, xTemp = data.generate_data(iList, flow, U0[:,k,None], control, Nx);
 
         uTrain[i:i+NuPH,:] = uTemp;
         xTrain[j:j+Nx,:] = xTemp;
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     NkH = obsH()['Nk'];
 
     # simulation variables and data gen.
-    T = 10;  Nt = round(T/dt)+1;
+    T = 1;  Nt = round(T/dt)+1;
     tList = [[i*dt for i in range(Nt)]];
 
 
@@ -76,24 +76,30 @@ if __name__ == "__main__":
     A = 10;
     N0 = 10;
     X0 = 2*A*np.random.rand(Nx,N0) - A;
-    X, Y, XU0 = createDynamicSets(tList, X0);
+    # X, Y, XU0 = createDynamicSets(tList, X0);
 
-    kxvar = kman.KoopmanOperator(obsXUH, obsXU);
-    Kx = kxvar.edmd(X, Y, XU0);
+    # kxvar = kman.KoopmanOperator(obsXUH, obsXU);
+    # kxvar = kxvar.edmd(X, Y, XU0);
 
-    print('Kx:\n', kxvar);
-    print('Kx.PsiX:\n', kxvar.K[:NkX,:].T);
-    print('Kx.PsiU:\n', kxvar.K[NkX:,:].T);
+    # print('Kx:\n', kxvar);
+    # print('Kx.PsiX:\n', kxvar.K[:NkX,:].T);
+    # print('Kx.PsiU:\n', kxvar.K[NkX:,:].T);
 
-    # evaluate the behavior of Kx with remeasurement function
-    x0ref = np.array( x0 )[:,None];
-    uref = np.array( [[1],[2]] );
-    xTest, PsiTest = posTrackingNoControl(tList, kxvar, x0ref, uref);
+    # # evaluate the behavior of Kx with remeasurement function
+    # x0ref = np.array( x0 )[:,None];
+    # uref = np.array( [[1],[2]] );
+    # xTest, PsiTest = posTrackingNoControl(tList, kxvar, x0ref, uref);
 
-    # plot test results
-    plotcomp(tList, xTest, PsiTest);
+    # # plot test results
+    # plotcomp(tList, xTest, PsiTest);
+    # plt.show();
 
 
     # control flow functions
     iList = [ [i for i in range(max_iter)] ];
-    createControlSets(iList, X0);
+    U1, U2, UX0 = createControlSets(iList, X0);
+
+    kuvar = kman.KoopmanOperator( obsH );
+    kuvar.edmd(U1, U2, UX0);
+
+    print('Ku:\n', kuvar);
--- diffdrive.py
+++ diffdrive.py
@@ -133,6 +133,95 @@ def cost(mpc_var, xlist, ulist):
     return C;
 
 
+# plot comparisons
+def posTrackingNoControl(tList, kxvar, x0ref, uref):
+    # dim variables
+    NkX = obsX()['Nk'];
+    NkU = obsU()['Nk'];
+
+    # evaluate the behavior of Kx with remeasurement function
+    dModel1 = lambda x: np.array( model(x,uref,None) ).reshape(Nx,1);
+    kModel1 = lambda Psi: kxvar.K@rmes(Psi);
+    def rmes(Psi):
+        # tweaks for initial tests
+        x = Psi[:Nx].reshape(Nx,1);
+        u = uref;
+        X = np.vstack( (x,u) );
+
+        PsiX = Psi[:NkX].reshape(NkX,1);
+        PsiU = Psi[NkX:].reshape(NkU,1);
+        PsiH = obsH(X);
+
+        Psin = np.vstack( (PsiX, np.kron(PsiU, PsiH)) );
+        return Psin;
+
+    # test data generation
+    Psi0 = obsXU( np.vstack( (x0ref, uref) ) );
+    PsiTest = data.generate_data(tList, kModel1, Psi0)[0];
+    xTest = data.generate_data(tList, dModel1, x0ref)[0];
+
+    return xTest, PsiTest;
+
+def plotcomp(tList, xTest, PsiTest, save=0):
+    # plot test results
+    fig, axs = plt.subplots(1,2);
+
+    axs[0].plot(xTest[0], xTest[1], label='Model');
+    axs[0].plot(PsiTest[0], PsiTest[1], linestyle='--', label='KCE');
+
+    axs[0].set_xlabel('$x_1$')
+    axs[0].set_ylabel('$x_2$')
+    axs[0].axis('equal');
+    axs[0].grid(1);
+    axs[0].legend();
+
+    # evaluate error
+    Ne = len( tList[0] ) - 1;
+    axs[1].plot([tList[0][0], tList[0][Ne]], [0,0], color='r', linestyle='--');
+    axs[1].plot(tList[0][:Ne], PsiTest[0,:Ne]-xTest[0,:Ne], label='$x_1$');
+    axs[1].plot(tList[0][:Ne], PsiTest[1,:Ne]-xTest[1,:Ne], label='$x_2$');
+    axs[1].plot(tList[0][:Ne], PsiTest[2,:Ne]-xTest[2,:Ne], label='$x_3$');
+
+    axs[1].set_ylabel('Error');
+    axs[1].set_xlabel('Time');
+    axs[1].set_ylim( (-1,1) );
+    axs[1].grid(1);
+    axs[1].legend();
+
+    fig.tight_layout();
+
+    # save results
+    if save:
+        figRes.savefig('/home/michaelnaps/prog/kman/.figures/donald.png', dpi=600);
+        figError.savefig('/home/michaelnaps/prog/kman/.figures/donaldError.png', dpi=600);
+
+
+# hyper parameter(s)
+pi = math.pi;
+PH = 10;
+kl = 2;
+Nx = 3;
+Nu = 2;
+R = 1/2;  # robot-body radius
+dt = 0.001;
+alpha = 0.01;
+
+# initialize states
+x0 = [0,0,0];
+xd = [0,0,0];
+uinit = [0 for i in range(Nu*PH)];
+
+# create MPC class variable
+dt_mpc = 0.01;
+model_type = 'discrete';
+max_iter = 100;
+params = Parameters(x0, xd, buffer_length=25);
+mpc_var = mpc.ModelPredictiveControl('ngd', model, cost, params, Nu,
+    num_ssvar=Nx, PH_length=PH, knot_length=kl, time_step=dt_mpc,
+    max_iter=max_iter, model_type=model_type);
+mpc_var.setAlpha(alpha);
+
+
 # observable functions
 def obsX(x=None):
     if x is None:
@@ -165,12 +254,14 @@ def obsXU(X=None, mvar=None):
 
 def obsH(X=None, mvar=None):
     if X is None:
-        meta = {'Nk':1+Nu};
+        meta = {'Nk':2*Nu*PH};
         return meta;
 
-    u = X[Nx:].reshape(Nu,1);
+    u = X[:Nu*PH].reshape(Nu*PH,);
+    x = X[Nu*PH:].reshape(Nx,);
+    g = np.array( mpc_var.gradient(x, u) );
 
-    PsiH = np.vstack( ([1], u) );
+    PsiH = np.vstack( (u[:,None], g[:,None]) );
     return PsiH;
 
 def obsXUH(X=None, mvar=None):
@@ -187,93 +278,3 @@ def obsXUH(X=None, mvar=None):
 
     PsiXUH = np.vstack( (PsiX, np.kron(PsiU, PsiH)) );
     return PsiXUH;
-
-
-# plot comparisons
-def posTrackingNoControl(tList, kxvar, x0ref, uref):
-    # dim variables
-    NkX = obsX()['Nk'];
-    NkU = obsU()['Nk'];
-
-    # evaluate the behavior of Kx with remeasurement function
-    dModel1 = lambda x: np.array( model(x,uref,None) ).reshape(Nx,1);
-    kModel1 = lambda Psi: kxvar.K@rmes(Psi);
-    def rmes(Psi):
-        # tweaks for initial tests
-        x = Psi[:Nx].reshape(Nx,1);
-        u = uref;
-        X = np.vstack( (x,u) );
-
-        PsiX = Psi[:NkX].reshape(NkX,1);
-        PsiU = Psi[NkX:].reshape(NkU,1);
-        PsiH = obsH(X);
-
-        Psin = np.vstack( (PsiX, np.kron(PsiU, PsiH)) );
-        return Psin;
-
-    # test data generation
-    Psi0 = obsXU( np.vstack( (x0ref, uref) ) );
-    PsiTest = data.generate_data(tList, kModel1, Psi0)[0];
-    xTest = data.generate_data(tList, dModel1, x0ref)[0];
-
-    return xTest, PsiTest;
-
-def plotcomp(tList, xTest, PsiTest, save=0):
-    # plot test results
-    figRes, axsRes = plt.subplots();
-
-    axsRes.plot(xTest[0], xTest[1], label='Model');
-    axsRes.plot(PsiTest[0], PsiTest[1], linestyle='--', label='KCE');
-
-    plt.xlabel('$x_1$')
-    plt.ylabel('$x_2$')
-    axsRes.axis('equal');
-    figRes.tight_layout();
-    axsRes.legend();
-    plt.grid();
-
-    # evaluate error
-    Ne = len( tList[0] ) - 1;
-    figError, axsError = plt.subplots();
-
-    axsError.plot([tList[0][0], tList[0][Ne]], [0,0], color='r', linestyle='--');
-    axsError.plot(tList[0][:Ne], PsiTest[0,:Ne]-xTest[0,:Ne], label='$x_1$');
-    axsError.plot(tList[0][:Ne], PsiTest[1,:Ne]-xTest[1,:Ne], label='$x_2$');
-    axsError.plot(tList[0][:Ne], PsiTest[2,:Ne]-xTest[2,:Ne], label='$x_3$');
-
-    axsError.set_ylim( (-0.2,0.2) );
-    axsError.grid();
-    axsError.legend();
-
-    # save results
-    if save:
-        figRes.savefig('/home/michaelnaps/prog/kman/.figures/donald.png', dpi=600);
-        figError.savefig('/home/michaelnaps/prog/kman/.figures/donaldError.png', dpi=600);
-    else:
-        plt.show();
-
-
-# hyper parameter(s)
-pi = math.pi;
-PH = 10;
-kl = 2;
-Nx = 3;
-Nu = 2;
-R = 1/2;  # robot-body radius
-dt = 0.001;
-alpha = 0.01;
-
-# initialize states
-x0 = [0,0,0];
-xd = [0,0,0];
-uinit = [0 for i in range(Nu*PH)];
-
-# create MPC class variable
-dt_mpc = 0.01;
-model_type = 'discrete';
-max_iter = 100;
-params = Parameters(x0, xd, buffer_length=25);
-mpc_var = mpc.ModelPredictiveControl('ngd', model, cost, params, Nu,
-    num_ssvar=Nx, PH_length=PH, knot_length=kl, time_step=dt_mpc,
-    max_iter=max_iter, model_type=model_type);
-mpc_var.setAlpha(alpha);
