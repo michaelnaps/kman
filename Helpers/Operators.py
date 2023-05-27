8126f622afd642bc889f3180385913558d03263b
Michael Napoli <mnapoli@irobot.com>
2023-05-26 22:50:48 -0400

    add obs class to operators

---
+++ Helpers/Operators.py
@@ -0,0 +1,13 @@
+import numpy as np
+from LearningStrategies import *
+
+def cascade_edmd(Klist, Tlist, X, Y, X0):
+	pass;
+
+class Observables:
+	def __init__(self, obs):
+		pass;
+
+class KoopmanOperator(LearningStrategies):
+	def __init__(self, obsX, obsY=None, K=None, T=None):
+		pass;
