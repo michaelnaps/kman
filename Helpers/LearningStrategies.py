d310528b81433ba79f6cc8d96ea20420b3db58cf
Michael Napoli <mnapoli@irobot.com>
2023-05-27 12:53:40 -0400

    added unmodified generate_data function to LS

--- Helpers/LearningStrategies.py
+++ Helpers/LearningStrategies.py
@@ -6,6 +6,46 @@
 
 import numpy as np
 
+# Helper data function for generating sets.
+def generate_data(tlist, model, X0, control=None, Nu=0):
+    Nx = len(X0);
+    N0 = len(X0[0]);
+    Nt = len(tlist[0]);
+
+    if control is not None:
+        ulist = np.empty( (N0*Nu, Nt-1) );
+    else:
+        ulist = None;
+        Nu = Nx;
+
+    xlist = np.empty( (N0*Nx, Nt) );
+
+    n = 0;
+    k = 0;
+    for i in range(N0):
+
+        if control is not None:
+            u = np.empty( (Nu, Nt-1) );
+        x = np.empty( (Nx, Nt) );
+
+        x[:,0] = X0[:,i];
+
+        for t in range(Nt-1):
+            if control is not None:
+                u[:,t] = control(x[:Nx,t]).reshape(Nu,);
+                x[:,t+1] = model(x[:Nx,t], u[:,t]).reshape(Nx,);
+            else:
+                x[:,t+1] = model(x[:Nx,t]).reshape(Nx,);
+
+        if control is not None:
+            ulist[k:k+Nu,:] = u;
+            k = k + Nu;
+
+        xlist[n:n+Nx,:] = x;
+        n = n + Nx;
+
+    return xlist, ulist;
+
 # Class: DataSet
 # Assumption: Uniform series/stacked data structure.
 class StateDataSet:
