fd4f2eb07f2c90b5744c41538c55075d84b71be8
Michael Napoli <mnapoli@irobot.com>
2023-05-29 18:53:40 -0400

    started entering test model, etc.

--- lietest.py
+++ lietest.py
@@ -3,5 +3,18 @@ import numpy as np
 from Helpers.LearningStrategies import *
 from Helpers.Operators import *
 
+# hyper paramters
+A = 10;
+Nx = 2;
+
+# model function: continuous, second-order
+def model(x):
+	dx = np.array( [
+		x[0],
+		x[1] - x[0]**2
+	] );
+	return dx;
+
 if __name__=="__main__":
-	pass;
+	N0 = 2;
+	X0 = 2*A*p.random.rand(Nx, N0) - A;
