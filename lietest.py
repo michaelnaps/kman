f214fdc1102f5802fc87461d701a0082342c9a4e
Michael Napoli <mnapoli@irobot.com>
2023-06-03 21:58:06 -0400

    plot edit

--- lietest.py
+++ lietest.py
@@ -73,15 +73,15 @@ if __name__=="__main__":
 	# Plot results of the simulation.
 	fig, axs = plt.subplots( 3,1 );
 	axs[0].plot(tList[0], xList[0], label='Model');
-	axs[0].plot(tList[0], psiList[0], label='Lie');
+	axs[0].plot(tList[0], psiList[0], linestyle='--', label='Lie');
 	axs[0].set_title( '$x_1$' );
 
 	axs[1].plot(tList[0], xList[1], label='Model');
-	axs[1].plot(tList[0], psiList[1], label='Lie');
+	axs[1].plot(tList[0], psiList[1], linestyle='--', label='Lie');
 	axs[1].set_title( '$x_2$' );
 
 	axs[2].plot(tList[0], xList[0]**2, label='Model');
-	axs[2].plot(tList[0], psiList[2], label='Lie');
+	axs[2].plot(tList[0], psiList[2], linestyle='--', label='Lie');
 	axs[2].set_title( '$x_1^2$' );
 
 	axs[0].legend();
