bc3efd390ab3b5a851d2748381313f554a5c50ff
Michael Napoli <mnapoli@irobot.com>
2023-07-10 17:48:02 -0400

    added cos and smooth wave function

--- FourierSeries/test.py
+++ FourierSeries/test.py
@@ -9,24 +9,27 @@ import matplotlib.pyplot as plt
 from KMAN.Operators import *
 
 # Hyper parameter(s).
-N = 100;
+N = 5;
 # N = 1000;
 # N = 10000;  # Near perfect...
-beta = 0.001;
+beta = 0.01;
 
 # Square wave initialization.
 def wave(x):
-    return np.sign( x );
+    return 0.50*np.sin( x ) + 0.15*np.sin( x )**2 + 0.25*np.cos( x )**3;
 
 # Observables
 def obsX(x=None):
     if x is None:
-        meta = {'Nk':N};
+        meta = {'Nk':2*N};
         return meta;
 
-    Psi = np.empty( (N,1) );
+    k = 0;
+    Psi = np.empty( (2*N,1) );
     for i in range( N ):
-        Psi[i] = np.sin( i*x );
+        Psi[k] = np.sin( i*x );
+        Psi[k+1] = np.cos( i*x );
+        k = k + 2;
 
     return Psi;
 
@@ -38,9 +41,9 @@ def obsY(x=None):
 
 if __name__ == '__main__':
     # Generate x-data for square wave.
-    T = 1;  Nt = round( T/beta ) + 1;
+    T = 10;  Nt = round( T/beta ) + 1;
     X = np.array( [[beta*(i-Nt) for i in range( 2*Nt )]] );
-    Y = np.sign( X );
+    Y = wave( X );
 
     # Solve using Koopman operator class.
     kvar = KoopmanOperator( obsX, obsY=obsY );
@@ -52,5 +55,5 @@ if __name__ == '__main__':
     # Plot results.
     fig, axs = plt.subplots();
     axs.plot( X.T, Y.T, label='True' );
-    axs.plot( X.T, Ytest[0,:].T, label='Fourier' );
+    axs.plot( X.T, Ytest[0,:].T, linestyle='--', label='Fourier' );
     plt.show();
