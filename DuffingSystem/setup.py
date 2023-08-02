2b4bb633d0b9df159dcf482272778de21ccdce9f
Michael Napoli <mikednapoli@gmail.com>
2023-08-01 22:45:18 -0400

    playing with sim, shortened tim-step but skipping steps in sim

--- DuffingSystem/setup.py
+++ DuffingSystem/setup.py
@@ -11,31 +11,28 @@ from GEOM.Vehicle2D import *
 
 # Hyper parameter(s).
 dt = 0.001
-Nx = 3
+Nx = 2
 
 # Duffing model.
 def model(x) -> np.array:
-    delta = 0.02
-    alpha = 1
-    theta = 5
-    gamma = 8
-    omega = 0.5
+    alpha = 5
+    delta = 0.2
+    gamma = 1
     dx = np.array( [
         x[1],
-        gamma*np.cos( omega*x[2] ) - (delta*x[1] + alpha*x[0] + theta*x[0]**3),
-        1
+        alpha*x[0] - delta*x[0]**3 - gamma*x[1]
     ] )
     return x + dt*dx
 
 # Observation spaces of interest.
 def obsx(x=None):
-    Ntr = 2
+    P = 7
     if x is None:
-        return {'Nk':3*Nx+Ntr+1}
+        return {'Nk':P*Nx+1}
     N = x.shape[1]
-    x2 = x**2
-    x3 = x**3
-    psi = np.vstack( (x, x2, x3, np.sin( x[2] ), np.cos( x[2] ), np.ones( (1,N) )) )
+    x1P = [ x[0]**i for i in range( 2,P+1 ) ]
+    x2P = [ x[1]**i for i in range( 2,P+1 ) ]
+    psi = np.vstack( (x, x1P, x2P, np.ones( (1,N) )) )
     return psi
 
 # Main execution block.
@@ -43,7 +40,10 @@ if __name__ == '__main__':
     # Initial positions.
     A = 2.5
     N0 = 10
-    X0 = 2*A*np.random.rand( Nx, N0 ) - A
+    X0 = np.vstack( (
+        np.linspace( -A, A, N0 ),
+        np.zeros( (1, N0) )
+    ) )
 
     # Generate trajectory data.
     T = 10;  Nt = round( T/dt ) + 1
@@ -61,26 +61,26 @@ if __name__ == '__main__':
     # Plot comparison results.
     fig, axs = plt.subplots()
     trueSwm = Swarm2D( X0[:2], fig=fig, axs=axs, zorder=5,
-        radius=0.10, tail_length=100 )
-    kmanSwm = Swarm2D( X0[:2], fig=fig, axs=axs, zorder=1,
-        radius=0.15, color='indianred', tail_length=100 )
+        radius=0.10, tail_length=500 )
+    # kmanSwm = Swarm2D( X0[:2], fig=fig, axs=axs, zorder=1,
+    #     radius=0.15, color='indianred', tail_length=100 )
     trueSwm.draw()
-    kmanSwm.draw()
+    # kmanSwm.draw()
 
-    plt.axis( [-10,10,-10,10] )
+    plt.axis( [-6, 6, -6, 6] )
     plt.gca().set_aspect( 'equal', adjustable='box' )
     plt.show( block=0 )
 
     input( "Press ENTER to begin simulation..." )
     psi = obsx( X0 )
-    for x in xList.T:
-        # Update true swarm based on set.
-        trueSwm.update( x.reshape( N0, Nx ).T[:2] )
-
-        # Update Koopman operator apprx.
+    for i, x in enumerate( xList.T ):
         psi = kman.K@psi
-        kmanSwm.update( psi[:2] )
 
-        # Pause sim.
-        plt.pause( 1e-3 )
+        # Update simulation based on sets.
+        if i % 10 == 0:
+            trueSwm.update( x.reshape( N0, Nx ).T[:2] )
+            # kmanSwm.update( psi[:2] )
+
+            # Pause sim. for visualization.
+            plt.pause( 1e-3 )
     input( "Press ENTER to exit program..." )
