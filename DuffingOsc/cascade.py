2f7e44c6a562a4c540043047a4c1f1ad0825776e
Michael Napoli <mikednapoli@gmail.com>
2023-08-15 22:03:51 -0400

    added sim for single point

--- DuffingOsc/cascade.py
+++ DuffingOsc/cascade.py
@@ -60,13 +60,12 @@ def obs123(X=None):
     psi = np.vstack( (psi1, psi2, psi3) )
     return psi
 
-def obs123p(X=None):
+def obs12(X=None):
     if X is None:
-        return {'Nk': obs1()['Nk']+obs2()['Nk']+obs3p()['Nk']}
+        return {'Nk': obs1()['Nk']+obs2()['Nk']}
     psi1 = obs1( X )
     psi2 = obs2( X )
-    psi3p = obs3p( X )
-    psip = np.vstack( (psi1, psi2, psi3p) )
+    psip = np.vstack( (psi1, psi2) )
     return psip
 
 
@@ -106,7 +105,7 @@ if __name__ == '__main__':
     # Initialize operator variables and solve.
     k3var = KoopmanOperator( obs3, obs3p )
     k2var = KoopmanOperator( obs2 )
-    k1var = KoopmanOperator( obs123, obs123p, T=shift( (k2var, k3var) ) )
+    k1var = KoopmanOperator( obs123, obs12, T=shift( (k2var, k3var) ) )
 
     Klist = (k1var, k2var, k3var)
     Tlist = (shift, None)
@@ -117,7 +116,59 @@ if __name__ == '__main__':
         print( K )
 
     # Calculate cumulative operator.
-    kvar = KoopmanOperator( obs123, obs123p )
+    kvar = KoopmanOperator( obs123, obs12 )
     kvar.setOperator( Klist[0].K@shift( Klist[1:] ) )
     kvar.resError( xTrain, yTrain, save=1 )
     print( 'Cumulative operator:\n', kvar )
+
+    # Start simulation?
+    ans = input("\n\nPress ENTER to begin simulation... ")
+    if ans == 'n':
+        exit()
+
+    # Simulate results.
+    def rmes(psi):
+        x = psi[:Nx-1]
+        psi3 = obs3( x )
+        psi = np.vstack( (psi, psi3) )
+        return psi
+
+    # Position Initializations.
+    As = 1.5
+    N0s = 1
+    Xs = 2*As*np.random.rand( Nx, N0s ) - As
+    PSIs = obs12( Xs )
+
+    # Vehicle/plot initializations.
+    fig, axs = plt.subplots()
+    trueSwrm = Swarm2D( Xs[:2], fig=fig, axs=axs, zorder=5,
+        radius=0.10, tail_length=500 )
+    kmanSwrm = Swarm2D( PSIs[:2], fig=fig, axs=axs, zorder=1,
+        radius=0.15, color='indianred', tail_length=500 )
+    trueSwrm.draw()
+    kmanSwrm.draw()
+
+    # Final adjustments and show plot.
+    plt.axis( [-2, 2, -2, 2] )
+    plt.gca().set_aspect( 'equal', adjustable='box' )
+    plt.show( block=0 )
+
+    # Simulation step freq.
+    dtmin = 0.1
+    if dt < dtmin:
+        n = round( dtmin/dt )
+    else:
+        n = 1
+
+    # Simulation time parameters.
+    T = 100;  Nt = round( T/dt ) + 1
+    for i in range( Nt ):
+        # Get new positions.
+        Xs = model( Xs[:,0] )[:,None]
+        PSIs = kvar.K@rmes( PSIs )
+
+        # Update plots for appropriate time-steps.
+        if i % n == 0:
+            trueSwrm.update( Xs[:2] )
+            kmanSwrm.update( PSIs[:2] )
+            plt.pause( 1e-3 )
