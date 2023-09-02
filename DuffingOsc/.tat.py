28dbc558ad2755d6eb3db379049b8eeefde49651
Michael Napoli <mikednapoli@gmail.com>
2023-09-02 19:43:30 -0400

    added more bodies

--- DuffingOsc/.tat.py
+++ DuffingOsc/.tat.py
@@ -8,27 +8,33 @@ from GEOM.Vehicle2D import *
 # Main execution block.
 if __name__ == '__main__':
     # Simulation length.
-    T = 100;  dt = 0.001
+    T = 10.0;  dt = 0.001
     Nt = round( T/dt ) + 1
     tList = np.array( [ [i*dt for i in range( Nt )] ] )
 
     # Initial condtions.
-    N0 = 5
+    N0 = 9
     dX = 4*np.array( [
-        [0, 1, 0, -1, 0],
-        [0, 0, 1, 0, -1]
-    ] )
-    X0 = np.pi/2*np.array( [
-        [0, 1, 1, -1, -1],
-        [0, 1, -1, 1, -1],
-        [0 for i in range( N0 )]
-    ] )
+        [0, 1, 0, -1, 0, 1, 1, -1, -1],
+        [0, 0, 1, 0, -1, 1, -1, -1, 1] ] )
+    X0 = np.pi/2*np.hstack( (
+        np.array( [
+            [0, 1, 1, -1, -1],
+            [0, 1, -1, 1, -1],
+            [0 for i in range( 5 )] ] ),
+        np.vstack( (
+            np.random.rand( 2, 4 ),
+            [0 for i in range( 4 )]
+        ) ) ) )
+
+    print( X0 )
 
     # Plot vehicles.
     Ntail = round( Nt/25 )
     fig, axs = plt.subplots()
     swrm = Swarm2D( X0[:2]+dX, fig=fig, axs=axs, color='k',
-        radius=0.05, tail_length=Ntail ).draw()
+        radius=0.05, tail_length=Ntail )
+    swrm.setLineWidth( 1.0 ).draw()
 
     # Final adjustments and show plot.
     plt.axis( np.array( [min(dX[0]-3), max(dX[0]+3), min(dX[1]-3), max(dX[1]+3)] ) )
@@ -49,3 +55,4 @@ if __name__ == '__main__':
         if i % n == 0:
             swrm.update( X[:2]+dX )
             plt.pause( 1e-3 )
+    input( "Press ENTER to exit program... " )
