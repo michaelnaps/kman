dfd5a3a023e1a14e8ce07963e39a8d1d06f18051
Michael Napoli <mikednapoli@gmail.com>
2023-09-03 12:16:37 -0400

    added image

--- DuffingOsc/.tat.py
+++ DuffingOsc/.tat.py
@@ -8,7 +8,7 @@ from GEOM.Vehicle2D import *
 # Main execution block.
 if __name__ == '__main__':
     # Simulation length.
-    T = 10.0;  dt = 0.001
+    T = 50.0;  dt = 1e-3
     Nt = round( T/dt ) + 1
     tList = np.array( [ [i*dt for i in range( Nt )] ] )
 
@@ -27,14 +27,12 @@ if __name__ == '__main__':
             [0 for i in range( 4 )]
         ) ) ) )
 
-    print( X0 )
-
     # Plot vehicles.
     Ntail = round( Nt/25 )
     fig, axs = plt.subplots()
-    swrm = Swarm2D( X0[:2]+dX, fig=fig, axs=axs, color='k',
-        radius=0.05, tail_length=Ntail )
-    swrm.setLineWidth( 1.0 ).draw()
+    axs.scatter( X0[0]+dX[0], X0[1]+dX[1], marker='o', edgecolor='k', facecolor='none' )
+    swrm = Swarm2D( X0[:2]+dX, fig=fig, axs=axs, color='k', radius=0.01, tail_length=Ntail )
+    swrm.setLineWidth( 1.5 ).draw()
 
     # Final adjustments and show plot.
     plt.axis( np.array( [min(dX[0]-3), max(dX[0]+3), min(dX[1]-3), max(dX[1]+3)] ) )
@@ -51,8 +49,9 @@ if __name__ == '__main__':
     # Simulation loop.
     X = X0
     for i in range( Nt ):
+        # X = model3( X, dt=dt )
         X = model3( X, c=[1,1,1,1,1,0], dt=dt )
         if i % n == 0:
             swrm.update( X[:2]+dX )
-            plt.pause( 1e-3 )
+            # plt.pause( 1e-3 )
     input( "Press ENTER to exit program... " )
