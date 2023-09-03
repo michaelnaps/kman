9e62f6aef25345d19b4db8d809c6761f62dbeeed
Michael Napoli <mikednapoli@gmail.com>
2023-09-03 19:03:45 -0400

    created vdp file

--- VanDerPol/.tat.py
+++ VanDerPol/.tat.py
@@ -2,8 +2,24 @@ import sys
 from os.path import expanduser
 sys.path.insert( 0, expanduser('~')+'/prog/geom' )  # Plotting and sim classes.
 
-from duffing import *
 from GEOM.Vehicle2D import *
+
+
+# Hyper parameter(s)
+mu = 1
+
+
+# Van der Pol oscilator.
+def model(X, mu=1, dt=1e-3):
+    N = X.shape[1]
+    dX = np.empty( (2, N) )
+    for i, x in enumerate( X.T ):
+        dX[:,i] = np.array( [
+            x[1],
+            mu*(x[1] - x[1]*x[0]**2) - x[0]
+        ] )
+    return X + dt*dX
+
 
 # Main execution block.
 if __name__ == '__main__':
@@ -14,18 +30,16 @@ if __name__ == '__main__':
 
     # Initial condtions.
     N0 = 9
-    dX = 4*np.array( [
+    dX = mu*6*np.array( [
         [0, 1, 0, -1, 0, 1, 1, -1, -1],
         [0, 0, 1, 0, -1, 1, -1, -1, 1] ] )
     X0 = np.pi/2*np.hstack( (
         np.array( [
-            [0, 1, 1, -1, -1],
-            [0, 1, -1, 1, -1],
-            [0 for i in range( 5 )] ] ),
-        np.vstack( (
-            np.random.rand( 2, 4 ),
-            [0 for i in range( 4 )]
-        ) ) ) )
+            [1e-6, 1, 1, -1, -1],
+            [0, 1, -1, 1, -1]
+        ] ),
+        np.random.rand( 2, 4 )
+    ) )
 
     # Plot vehicles.
     Ntail = round( Nt/25 )
@@ -35,8 +49,7 @@ if __name__ == '__main__':
     swrm.setLineWidth( 1.5 ).draw()
 
     # Final adjustments and show plot.
-    delta = 2
-    plt.axis( np.array( [min(dX[0]-delta), max(dX[0]+delta), min(dX[1]-delta), max(dX[1]+delta)] ) )
+    plt.axis( np.array( [min(dX[0]-3), max(dX[0]+3), min(dX[1]-3), max(dX[1]+3)] ) )
     plt.gca().set_aspect( 'equal', adjustable='box' )
     plt.show( block=0 )
 
@@ -51,7 +64,7 @@ if __name__ == '__main__':
     X = X0
     for i in range( Nt ):
         # X = model3( X, dt=dt )
-        X = model3( X, c=[1,1,1,1,1,0], dt=dt )
+        X = model( X, mu=mu, dt=dt )
         if i % n == 0:
             swrm.update( X[:2]+dX )
             # plt.pause( 1e-3 )
