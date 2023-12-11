9256087fe38fda4b96f87cd5d6f3594dab528740
Michael Napoli <mikednapoli@gmail.com>
2023-12-10 22:26:48 -0500

    def. step size was too large

--- flow/nonconvex.py
+++ flow/nonconvex.py
@@ -32,21 +32,19 @@ def observe(x=None):
 if __name__ == '__main__':
     # Optimization variable.
     eps = 1e-21
-    alpha = 1e-9
+    alpha = 1e-3
     optvar = Optimizer( cost, eps=eps )
     optvar.setStepSize( alpha ).setMaxIter( np.inf )
 
     # Initial guess and system size.
-    p = 2
-    A = 10
-    X0 = np.array( [
-        [[0],[0]],
-        [[3.01],[2.01]]
-    ] )
+    p = 1
+    A = 1
+    X0 = np.array( [ [[3],[2]] ] ) \
+        + 2*A*np.random.rand( n,p ) - A
 
     # Solve optimization problem and save steps.
     XList = []
-    qmax = np.inf
+    qmax = 1000
     for x0 in X0:
         x = x0
         xList = [x]
@@ -54,14 +52,14 @@ if __name__ == '__main__':
         gnorm = np.linalg.norm( dg )
         q = 0
         while gnorm > 1e-21 and q < qmax:
-            print( gnorm )
             x = optvar.step( x, dg )
             xList = xList + [x]
             dg = fdm2c( cost, x )
             gnorm = np.linalg.norm( fdm2c( cost, x ) )
             q += 1
         XList = XList + [np.hstack( xList )]
-    print( [xList[:,-1] for xList in XList] )
+        print( 'Complete for x0:', x0.T )
+    print( 'xList:', [xList[:,-1] for xList in XList] )
 
     # Create snapshot lists.
     X = np.hstack( [xList[:,:-1] for xList in XList] )
