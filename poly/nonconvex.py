058b2286bcda1e923c58fa85b357889235fedd40
Michael Napoli <mikednapoli@gmail.com>
2024-01-01 22:44:06 -0500

    writing koopman sub-domain example

--- poly/nonconvex.py
+++ poly/nonconvex.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 from KMAN.Operators import *
 from MPC.Optimizer import fdm2c
 
+A = 1.50
+p = 0.56
 n = 1
 m = 10
-A = 0.001
 
 def polyn(x):
     return x**4 - 3*x**3 + x**2 + x
@@ -19,14 +20,23 @@ def model(x):
     dx = fdm2c( polyn, x )
     return x - alpha*dx
 
+def obs(x=None):
+    if x is None:
+        return {'Nk':2}
+    m = x.shape[1]
+    return np.vstack( (x, np.ones( (1,m) )) )
+
 if __name__ == '__main__':
     # Initial positions.
-    X0 = (2*A*np.random.rand( n, m ) - A) + 0.56
+    X0 = (2*A*np.random.rand( n, m ) - A) + p
+    I0 = ['1' if x0 < p else '2' for x0 in X0.T]
     print( 'X0:', X0 )
+    print( 'I0:', I0 )
 
-    # Simulation through initial positions.
-    Xdata = []
-    for x0 in X0.T:
+    # Simulation block.
+    X1data = []
+    X2data = []
+    for x0, i0 in zip( X0.T, I0 ):
         x = x0[:,None]
         Xsim = [x]
         xnorm = np.linalg.norm( fdm2c( polyn, x0 ) )
@@ -34,7 +44,30 @@ if __name__ == '__main__':
             x = model( x )
             xnorm = np.linalg.norm( fdm2c( polyn, x ) )
             Xsim = Xsim + [x]
-        Xdata = Xdata + [np.hstack( Xsim )]
+        if i0 == '1':
+            X1data = X1data + [np.hstack( Xsim )]
+        else:
+            X2data = X2data + [np.hstack( Xsim )]
+
+    # Form training sets.
+    X1 = np.hstack( [X[:,:-1] for X in X1data] )
+    Y1 = np.hstack( [X[:,1:] for X in X1data] )
+    X2 = np.hstack( [X[:,:-1] for X in X2data] )
+    Y2 = np.hstack( [X[:,1:] for X in X2data] )
+
+    print( X1 )
+    print( Y1 )
+
+    # Kman variables.
+    k1var = KoopmanOperator( obs )
+    k2var = KoopmanOperator( obs )
+
+    # Solve for Koopman operators on sub-domains.
+    k1var.dmd( X1, Y1 )
+    k2var.dmd( X2, Y2 )
+
+    print( 'K1:', k1var )
+    print( 'K2:', k2var )
 
     # Plot results.
     fig, axs = plt.subplots()
@@ -45,9 +78,11 @@ if __name__ == '__main__':
     axs.plot( Xlist, Ylist, color='k', linewidth=3 )
 
     # Plot gradient descent data.
-    for X in Xdata:
+    for X in X1data:
+        axs.plot( X.T, polyn( X ).T )
+    for X in X2data:
         axs.plot( X.T, polyn( X ).T )
 
     # Show finished plot.
     axs.grid( 1 )
-    plt.show()
+    # plt.show()
