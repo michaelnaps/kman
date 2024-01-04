bb3ff27581d01bafde14c2dfcf5dd7735e2371d4
Michael Napoli <mikednapoli@gmail.com>
2024-01-04 14:30:25 -0500

    save indices not initial positions

--- tensor/himmel.py
+++ tensor/himmel.py
@@ -15,11 +15,16 @@ from MPC.Optimizer import *
 # Set global number print setting.
 np.set_printoptions(precision=3, suppress=True, linewidth=np.inf)
 
-# Dimension of system.
+# Dimension of system and other parameter(s).
 n = 2
+m = 100
 alpha = 1e-3
 gamma = alpha
 beta = 100
+
+# Local maximum and offset.
+A = 2
+xmax = np.array( [[-0.270845],[-0.923039]] )
 
 # Nonconvex objective function.
 def cost(x):
@@ -48,64 +53,60 @@ def model(x):
 def observe(x=None):
     p = 5
     if x is None:
-        return {'Nk': (p + 1)*n + 1}
-    psi = np.vstack( [x]
-        + [gamma**(k+1)*costgradprop( x, p=k ) for k in range( p )]
-        + [1] )
+        return {'Nk': n+1}
+    psi = np.vstack( [x] + [1] )
     return psi
 
 # Main execution block.
 if __name__ == '__main__':
     # Optimization variable.
-    eps = 1e-21
+    eps = 1e-3
     optvar = Optimizer( cost, eps=eps )
     optvar.setStepSize( alpha ).setMaxIter( np.inf )
 
     # Initial guess and system size.
-    p = 4
-    A = 2.5
-    X0 = 2*A*np.random.rand( p,n,1 ) - A
+    X0 = (2*A*np.random.rand( n,m ) - A) + xmax
 
     # Solve optimization problem and save steps.
-    kmax = 1000
-    XList = []
-    for x0 in X0:
-        x = x0
+    kmax = 10000
+    Xlist = []
+    for x0 in X0.T:
+        x = x0[:,None]
         dg = fdm2c( cost, x )
-        gnorm = np.linalg.norm( dg )
-        xList = np.nan*np.ones( (n,kmax+1) )
-        xList[:,0] = x[:,0]
+        X = np.nan*np.ones( (n,kmax+1) )
+        X[:,0] = x[:,0]
         for k in range( kmax ):
             x = optvar.step( x, dg )
             dg = fdm2c( cost, x )
-            gnorm = np.linalg.norm( fdm2c( cost, x ) )
-            xList[:,k+1] = x[:,0]
-            if gnorm < eps: break
-        XList = XList + [xList[:,:k+1]]
-        print( 'Complete for x0 (%s):' % k, x0.T, '\t->\t', x.T )
+            X[:,k+1] = x[:,0]
+            if np.linalg.norm( costgrad( x ) ) < eps:
+                break
+        Xlist = Xlist + [X[:,:k+1]]
+        # print( 'Complete for x0 (%s):' % k, x0.T, '\t->\t', x.T )
+
+    # Group initial conditions by their fixed points.
+    xmins = np.array( [
+        [3,-2.805118,-3.779310, 3.584428],
+        [2, 3.131312,-3.283186,-1.848126]
+    ] )
+    fpsets = [[],[],[],[]]
+    for i, X in enumerate( Xlist ):
+        for j, xmin in enumerate( xmins.T ):
+            if np.linalg.norm( X[:,-1] - xmin ) < eps:
+                fpsets[j] = fpsets[j] + [X0[:,i,None]]
+
+    # Convert fixed point sets to numpy arrays.
+    for
 
     # Create snapshot lists.
-    X = np.hstack( [xList[:,:-1] for xList in XList] )
-    Y = np.hstack( [xList[:,1:] for xList in XList] )
+    Xtrain = np.hstack( [ for fp in fpsets] )
+    Ytrain = [fp[0][:,1:] for fp in fpsets]
 
     # Solve for Koopman operator.
-    kvar = KoopmanOperator( observe )
-    print( kvar.edmd( X, Y ) )
-
-    # Test Koopman operator on training set.
-    PSIList = []
-    for x0 in X0:
-        psi = kvar.obsX.lift( x0 )
-        psiList = np.empty( (observe()['Nk'], kmax+1) )
-        psiList[:,0] = psi[:,0]
-        for k in range( kmax ):
-            psi = kvar.K@psi
-            psiList[:,k+1] = psi[:,0]
-            if np.linalg.norm( psi ) > 15:
-                print('Operator dynamics diverged.')
-                break
-        PSIList = PSIList + [psiList]
-        print( 'Complete for x0:', x0.T, '\t->\t', psi[:n].T )
+    kvarlist = [KoopmanOperator( observe ).edmd( X, Y )
+        for X, Y in zip( Xtrain, Ytrain )]
+    for i, kvar in enumerate( kvarlist ):
+        print( 'K%s:' % i, kvar )
 
     # Initialize plot variables.
     fig, axs = plt.subplots()
@@ -117,20 +118,20 @@ if __name__ == '__main__':
     yRange = np.linspace( yBound[0], yBound[1], 1000 )
     xMesh, yMesh = np.meshgrid( xRange, yRange )
     gMesh = np.vstack( [
-        cost( np.vstack( (xList, yList) ) )
-            for xList, yList in zip( xMesh, yMesh ) ] )
+        cost( np.vstack( (xlist, ylist) ) )
+            for xlist, ylist in zip( xMesh, yMesh ) ] )
     levels = [1, 5] + [eta*(i + 1) for i in range( round( np.max( gMesh )/eta ) )]
     axs.contour( xMesh, yMesh, gMesh, levels=levels, colors='k' )
 
     # Add gradient descent results to plot.
-    for xList in XList:
-        axs.plot( xList[0,0], xList[1,0], marker='x', color='cornflowerblue' )
-        axs.plot( xList[0], xList[1], color='cornflowerblue' )
+    for xlist in Xlist:
+        axs.plot( xlist[0,0], xlist[1,0], marker='x', color='cornflowerblue' )
+        axs.plot( xlist[0], xlist[1], color='cornflowerblue' )
 
-    # Add operator results to plot.
-    for psiList in PSIList:
-        axs.plot( psiList[0,0], psiList[1,0], marker='x', color='indianred' )
-        axs.plot( psiList[0], psiList[1], color='indianred' )
+    # # Add operator results to plot.
+    # for psilist in PSIlist:
+    #     axs.plot( psilist[0,0], psilist[1,0], marker='x', color='indianred' )
+    #     axs.plot( psilist[0], psilist[1], color='indianred' )
 
     # Display plot.
     axs.set_aspect('equal', adjustable='box')
